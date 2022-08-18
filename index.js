43837b101f52a17915fd687fd71e550b5ded8498
Wilson-Ochieng <wilsonochieng718@gmail.com>
2022-08-18 08:37:25 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distanceinFeet){
+ if(distanceinFeet <= 400) {
+ return 'This one is on me!'
+ }
+ else if(distanceinFeet > 2000 && distanceinFeet<2500){
+ return 'I will gladly take your thirty bucks.'
+ }
+ else if(distanceinFeet > 2500){
+ return 'No can do.'
+    }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return  (city ==='NYC')? "Ok, sounds good." : "No go.";
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(reward){
+ switch(reward){
+      case "generous":
+        return "Thank you so much."
+  
+      case "not as generous":
+         return 'Thank you.'
+      default: 
+        return 'Bye.'
+ }
+  }
